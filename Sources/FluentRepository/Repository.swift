6fbb87ff6a7b21b82d54f0d8bd7ed9eb894a0808
Tony Arnold <tony@thecocoabots.com>
2024-06-14 22:43:59 +1000

    Minor tidying

--- Sources/FluentRepository/Repository.swift
+++ Sources/FluentRepository/Repository.swift
@@ -1,8 +1,10 @@
 import FluentKit
 
 public protocol Repository: Sendable {
-    /// The database that this repository will interact with.
+    /// The database that this repository will query and interact with.
     var database: Database { get }
-
+    
+    /// Initialize a new repository.
+    /// - Parameter database: The database that this repository will query and interact with.
     init(database: Database)
 }
