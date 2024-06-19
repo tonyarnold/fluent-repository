909633dbd04a756f5923fcd8b063d4688f14ab7a
Tony Arnold <tony@thecocoabots.com>
2024-06-19 22:28:00 +1000

    Don’t make the require extension available publicly

--- Sources/FluentRepository/Extensions/Model+Requires.swift
+++ Sources/FluentRepository/Extensions/Model+Requires.swift
@@ -1,7 +1,7 @@
 import FluentKit
 import Vapor
 
-public extension FluentKit.Model {
+extension FluentKit.Model {
     static func require(_ id: Self.IDValue, on database: Database) async throws -> Self {
         guard let entity = try await Self.find(id, on: database) else {
             throw Abort(.notFound, reason: "\(Self.self) with id \(id) not found")
